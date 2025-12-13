bc35950c9c8b22ce34b324f3155532b7cf474b45
Ahmad Masarrat <133361840+furzing@users.noreply.github.com>
2025-12-13 14:13:21 +0300

    cookies

--- server/index.ts
+++ server/index.ts
@@ -18,6 +18,7 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      sameSite: process.env.NODE_ENV === "production" ? 'lax' : 'strict',
     },
   })
 );
